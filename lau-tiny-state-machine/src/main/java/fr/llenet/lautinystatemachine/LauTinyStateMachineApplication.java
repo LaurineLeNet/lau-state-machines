3fb52d0ec566d949ae6ffc84c52cf36f3a043a9a
Laurine <lenetlaurine@gmail.com>
2024-04-22 12:55:21 +0200

    end of live coding

--- lau-tiny-state-machine/src/main/java/fr/llenet/lautinystatemachine/LauTinyStateMachineApplication.java
+++ lau-tiny-state-machine/src/main/java/fr/llenet/lautinystatemachine/LauTinyStateMachineApplication.java
@@ -1,16 +1,12 @@
 package fr.llenet.lautinystatemachine;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 @Log4j2
 public class LauTinyStateMachineApplication {
 
     public static void main(String[] args) {
         Engine engine = new Engine();
         engine.launch();
-
     }
 
 
